70583fa165850ba14cbcdaf50294bd8fc84027bf
Rafael de Conde <GitHubRafael@gmail.com>
2022-12-17 14:38:26 -0300

    Little repo clean up, no Rust code changes

--- compile.sh
+++ compile.sh
@@ -1,6 +1,11 @@
 SECONDS=0 
 echo generating artifacts
 pyoxidizer generate-python-embedding-artifacts pyembedded
+export VCPKG_ROOT="${CARGO_TARGET_DIR:-$(pwd)/target}/vcpkg"
+export RUSTFLAGS='-Ctarget-feature=+crt-static'  # Windows only
+export VCPKGRS_TRIPLET='x64-windows-static-release'  # Windows only
+export TECTONIC_DEP_BACKEND=vcpkg
+cargo vcpkg build
 echo compiling
 PYO3_CONFIG_FILE=$(pwd)/pyembedded/pyo3-build-config-file.txt PYTHONPATH=pyembedded/stdlib cargo run --release
 echo $SECONDS
