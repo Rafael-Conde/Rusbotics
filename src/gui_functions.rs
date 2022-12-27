0e1525374a617e0c800da273fc1530f70c1c0a71
Rafael de Conde <GitHubRafael@gmail.com>
2022-12-27 19:26:23 -0300

    Functionally placed into func & improved asthetics

--- src/gui_functions.rs
+++ src/gui_functions.rs
@@ -1,7 +1,16 @@
+#![warn(clippy::all,
+/*#![warn(*/clippy::pedantic,
+		clippy::perf,
+        clippy::nursery,
+        // clippy::cargo,
+        clippy::unwrap_used,
+        clippy::expect_used)]
+
+
 use crate::{extract_robot_data_from_file, script};
 use eframe::egui;
 use egui_extras::image::RetainedImage;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 
 pub trait Gui
 {
@@ -51,6 +60,37 @@ fn perform_calculations(path: String)
     script::get_matrix_image(joints);
 }
 
+
+fn button_generate_dh_matrix(picked_path: MutexGuard<String>, calculation_thread_state: Arc<Mutex<ThreadState>>, image_texture: Arc<Mutex<Option<RetainedImage>>>)
+{
+    let temp = (*picked_path).clone();
+    std::thread::spawn(move || {
+        perform_calculations(temp);
+        let path =  std::path::Path::new("test-page-0.png");
+        if path.exists()
+        {
+            if let Ok(image_file_bytes) = std::fs::read(path)
+            {
+                if let Ok(mut image_texture) = image_texture.lock()
+                {
+                    *image_texture = Some(RetainedImage::from_image_bytes("equacao", &image_file_bytes).unwrap());
+                }
+            }
+            else
+            {
+                println!("error while reading the image from the file");
+            }
+            if let Ok(mut state) = calculation_thread_state.lock()
+            {
+                *state = ThreadState::Finished;
+            };
+        }
+    });
+    // *current_thread_state = ThreadState::Running;
+}
+
+
+
 impl eframe::App for MyApp
 {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame)
@@ -88,39 +128,46 @@ impl eframe::App for MyApp
                                 {
                                     // teh deref is necessary
                                     // otherwise the Arc would've been copied
-                                    let temp = (*picked_path).clone();
+                                    // let temp = (*picked_path).clone();
                                     let calculation_thread_state =
                                         Arc::clone(&self.calculation_thread_state);
                                     let image_texture = Arc::clone(&self.image_texture);
-                                    std::thread::spawn(move || {
-                                        perform_calculations(temp);
-                                        let path =  std::path::Path::new("test-page-0.png");
-                                        if path.exists()
-                                        {
-                                            if let Ok(image_file_bytes) = std::fs::read(path)
-                                            {
-                                                if let Ok(mut image_texture) = image_texture.lock()
-                                                {
-                                                    *image_texture = Some(RetainedImage::from_image_bytes("equacao", &image_file_bytes).unwrap());
-                                                }
-                                            }
-                                            else
-                                            {
-                                                println!("error while reading the image from the file");
-                                            }
-                                            if let Ok(mut state) = calculation_thread_state.lock()
-                                            {
-                                                *state = ThreadState::Finished;
-                                            };
-                                        }
-                                    });
+                                    button_generate_dh_matrix(picked_path, calculation_thread_state, image_texture);
+                                //     std::thread::spawn(move || {
+                                //         perform_calculations(temp);
+                                //         let path =  std::path::Path::new("test-page-0.png");
+                                //         if path.exists()
+                                //         {
+                                //             if let Ok(image_file_bytes) = std::fs::read(path)
+                                //             {
+                                //                 if let Ok(mut image_texture) = image_texture.lock()
+                                //                 {
+                                //                     *image_texture = Some(RetainedImage::from_image_bytes("equacao", &image_file_bytes).unwrap());
+                                //                 }
+                                //             }
+                                //             else
+                                //             {
+                                //                 println!("error while reading the image from the file");
+                                //             }
+                                //             if let Ok(mut state) = calculation_thread_state.lock()
+                                //             {
+                                //                 *state = ThreadState::Finished;
+                                //             };
+                                //         }
+                                //     });
                                 }
                                 *current_thread_state = ThreadState::Running;
                             }
                         }
                         ThreadState::Running =>
                         {
-                            ui.add_enabled(false, egui::Button::new("Running calculations..."));
+                            ui.horizontal(|ui|
+                            {
+                                ui.add_enabled(false, egui::Button::new("Running calculations..."));
+                                ui.spinner();
+                            });
+                            // ui.add_enabled(false, egui::Button::new("Running calculations..."));
+                            // ui.add_enabled(false, egui::ProgressBar::new(0.99f32).desired_width(230f32).text("Running Calculations...").animate(true));
                             self.missing_image_warned = false;
                         }
                         ThreadState::Finished =>
@@ -129,34 +176,38 @@ impl eframe::App for MyApp
                             {
                                 if let Ok(picked_path) = picked_path.lock()
                                 {
-                                    // teh deref is necessary
-                                    // otherwise the Arc would've been copied
-                                    let temp = (*picked_path).clone();
                                     let calculation_thread_state =
                                         Arc::clone(&self.calculation_thread_state);
                                     let image_texture = Arc::clone(&self.image_texture);
-                                    std::thread::spawn(move || {
-                                        perform_calculations(temp);
-                                        let path =  std::path::Path::new("test-page-0.png");
-                                        if path.exists()
-                                        {
-                                            if let Ok(image_file_bytes) = std::fs::read(path)
-                                            {
-                                                if let Ok(mut image_texture) = image_texture.lock()
-                                                {
-                                                    *image_texture = Some(RetainedImage::from_image_bytes("equacao", &image_file_bytes).unwrap());
-                                                }
-                                            }
-                                            else
-                                            {
-                                                println!("error while reading the image from the file");
-                                            }
-                                            if let Ok(mut state) = calculation_thread_state.lock()
-                                            {
-                                                *state = ThreadState::Finished;
-                                            };
-                                        }
-                                    });
+                                    button_generate_dh_matrix(picked_path, calculation_thread_state, image_texture);
+                                    // teh deref is necessary
+                                    // otherwise the Arc would've been copied
+                                    // let temp = (*picked_path).clone();
+                                    // let calculation_thread_state =
+                                    //     Arc::clone(&self.calculation_thread_state);
+                                    // let image_texture = Arc::clone(&self.image_texture);
+                                    // std::thread::spawn(move || {
+                                    //     perform_calculations(temp);
+                                    //     let path =  std::path::Path::new("test-page-0.png");
+                                    //     if path.exists()
+                                    //     {
+                                    //         if let Ok(image_file_bytes) = std::fs::read(path)
+                                    //         {
+                                    //             if let Ok(mut image_texture) = image_texture.lock()
+                                    //             {
+                                    //                 *image_texture = Some(RetainedImage::from_image_bytes("equacao", &image_file_bytes).unwrap());
+                                    //             }
+                                    //         }
+                                    //         else
+                                    //         {
+                                    //             println!("error while reading the image from the file");
+                                    //         }
+                                    //         if let Ok(mut state) = calculation_thread_state.lock()
+                                    //         {
+                                    //             *state = ThreadState::Finished;
+                                    //         };
+                                    //     }
+                                    // });
                                 }
                                 *current_thread_state = ThreadState::Running;
                             }
