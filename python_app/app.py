084734d38af9b403c1d23168887f697d254bc0b4
Rafael de Conde <GitHubRafael@gmail.com>
2023-01-09 16:30:02 -0300

    Adding LICENSE

--- python_app/app.py
+++ python_app/app.py
@@ -1,3 +1,8 @@
+"""This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+Copyright (c) 2023 Rafael de Conde Reis. All rights reserved."""
+
 # 1° coluna = alpha, 2° coluna = ai, 3° coluna = di, 4° coluna = theta
 # joint é uma matriz para cada linha da tabela DH onde r signigica uma junta rotativa e p uma junta prismática
 # tabela_DH =([['alpha_1','a_1','d_1','theta_1'],
@@ -8,6 +13,10 @@ from library import *
 
 
 # tabela_DH = ([['0', 'a_1', '0', 'theta_1'],
+"""This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+Copyright (c) 2023 Rafael de Conde Reis. All rights reserved.
+file, You can obtain one at http://mozilla.org/MPL/2.0/."""
 #              ['0', 'a_2', '0', 'theta_2']])
 #              #['alpha_3','a_3','d_3','theta_3']])#,
 #              #['alpha_4','a_4','d_4','theta_4'],
